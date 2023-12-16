4c22e8ee77125b20c9474d3ef181f5a637af115b
BlagoTcherecharov <117592898+BlagoTcherecharov@users.noreply.github.com>
2023-12-16 11:07:27 +0200

    Update Server.java

--- Домашно/Version with more players/Server.java
+++ Домашно/Version with more players/Server.java
@@ -43,7 +43,7 @@ class ClientHandler implements Runnable {
     private Socket s;
     private int number;
     private AtomicBoolean gameWon;
-    private boolean finish = true;
+    private boolean finish = false;
 
     public ClientHandler(Socket s, int number, AtomicBoolean gameWon) {
         this.s = s;
@@ -65,7 +65,118 @@ class ClientHandler implements Runnable {
         while (number != guess && !gameWon.get()) {
             try {
                 PrintWriter pr = new PrintWriter(s.getOutputStream());
-                pr.println(gameWon.get());
+                pr.println(finish);
+                pr.flush();
+
+                InputStreamReader in = new InputStreamReader(s.getInputStream());
+                BufferedReader bf = new BufferedReader(in);
+                guess = Integer.parseInt(bf.readLine());
+            } catch (NumberFormatException | IOException e) {
+                e.printStackTrace();
+            }
+
+            if (guess == number) {
+                System.out.println("You have guessed the number!");
+                
+                // Use compareAndSet to safely set the value to true
+                if (!gameWon.get()) {
+                    boolean oldValue = gameWon.get();
+                    boolean newValue = true;
+                    gameWon.compareAndSet(oldValue, newValue);
+                }
+
+                break;
+            } else if (guess > number)
+                System.out.println("Secret number is lower than " + guess);
+            else
+                System.out.println("Secret number is higher than " + guess);
+        }
+        
+        if(number != guess)
+        {
+            finish = true;
+            PrintWriter pr;
+            try {
+                pr = new PrintWriter(s.getOutputStream());
+                pr.println(finish);
+                pr.flush();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+
+            System.out.println("Other player has guessed the number before you!");
+        }
+    }
+}import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Server 
+{
+    private static final int PORT = 8080;
+    private static final int MAX_THREADS = 10; 
+
+    public static void main(String[] args) throws IOException 
+    {
+        ServerSocket ss = new ServerSocket(PORT);
+        System.out.println("Server started");
+
+        AtomicBoolean gameWon = new AtomicBoolean(false);
+        ExecutorService executorService = Executors.newFixedThreadPool(MAX_THREADS);
+        Random randomNum = new Random();
+        int number = randomNum.nextInt(100);
+
+        try 
+        {
+            while (true) 
+            {
+                Socket s = ss.accept();
+                System.out.println("New client connected: " + s);
+
+                Runnable clientHandler = new ClientHandler(s, number, gameWon);
+                executorService.submit(clientHandler);
+            }
+        } 
+        finally 
+        {
+            executorService.shutdown();
+            ss.close();
+        }
+    }
+}
+
+// ClientHandler class
+class ClientHandler implements Runnable {
+    private Socket s;
+    private int number;
+    private AtomicBoolean gameWon;
+    private boolean finish = false;
+
+    public ClientHandler(Socket s, int number, AtomicBoolean gameWon) {
+        this.s = s;
+        this.number = number;
+        this.gameWon = gameWon;
+    }
+
+    @Override
+    public void run() {
+        int guess = 0;
+        try {
+            PrintWriter pr = new PrintWriter(s.getOutputStream());
+            pr.println(number);
+            pr.flush();
+        } catch (IOException e) {
+            System.out.println("PrintWriter error!");
+        }
+
+        while (number != guess && !gameWon.get()) {
+            try {
+                PrintWriter pr = new PrintWriter(s.getOutputStream());
+                pr.println(finish);
                 pr.flush();
 
                 InputStreamReader in = new InputStreamReader(s.getInputStream());
